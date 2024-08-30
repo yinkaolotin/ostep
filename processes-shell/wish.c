b3a1ee4f65564cb49acf490ff05d05c8a30872a9
olayinka <olayinka@obmondo.com>
2024-08-30 20:14:47 +0100

    clean up
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,15 +4,63 @@
 #include <unistd.h>
 #include <string.h>
 
+void parse_cmd_line(FILE *in, char **command)
+{
+        char **args;
+        size_t len = 0;
+        size_t argsn = 0;
+        size_t r = getline(command, &len, in);
+        if (r == -1)
+        {
+                printf("failed to get command line\n");
+                exit(1);
+        }
+        (*command)[r-1] = '\0';
+
+        char *token;
+        char *temp = strdup(*command);
+        if (temp == NULL)
+        {
+                printf("strdup command\n");
+                exit(1);
+        }
+
+        while((token = strsep(command, " ")) != NULL)
+        {
+                argsn++;
+        }
+        args = malloc(argsn - 1);
+        if (args == NULL)
+        {
+                printf("malloc args\n");
+                exit(1);
+        }
+
+        int i = 0;
+        while((token = strsep(&temp, " ")) != NULL)
+        {
+                if (i == 0)
+                {
+                        i++;
+                } else 
+                {
+                        (args)[i-1] = strdup(token);
+                        i++;
+                }
+        }
+
+        for(size_t i = 0; i < (argsn-1); i++)
+        {
+                printf("%s\n", args[i]);
+        }
+}
+
 int main(int argc, char *argv[])
 {
         (void)argc;
         (void)argv;
 
         char *command;
-        char **args;
-        size_t argsn = 0;
-        size_t len = 0;
         FILE *in;
 
         in = fdopen(STDIN_FILENO, "r");
@@ -26,40 +74,7 @@ int main(int argc, char *argv[])
         {
                 printf("wish> ");
 
-                size_t r = getline(&command, &len, in);
-                if (r == -1)
-                {
-                        printf("failed to get command line\n");
-                        exit(1);
-                }
-
-                command[r-1] = '\0';
-
-                char *token;
-                char *temp = strdup(command);
-                while((token = strsep(&command, " ")) != NULL)
-                {
-                        argsn++;
-                }
-                args = malloc(argsn - 1);
-
-                int i = 0;
-                while((token = strsep(&temp, " ")) != NULL)
-                {
-                        if (i == 0)
-                        {
-                                i++;
-                        } else 
-                        {
-                                args[i-1] = strdup(token);
-                                i++;
-                        }
-                }
-
-                for(size_t i = 0; i < (argsn-1); i++)
-                {
-                        printf("%s\n", args[i]);
-                }
+                parse_cmd_line(in, &command);
         }
         return 0;
 }
